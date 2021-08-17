d769806c25809a0867cecf63d13608b11730e399
Daniel Mistrik <33633302+DanielMistrik@users.noreply.github.com>
2021-08-17 11:54:08 +0200

    Minor changes to error checking. The search returned an incomplete mnemonic or address this had to be fixed.

--- ethMnemonic/MnemGen/static/EthAddressToMnemonic.js
+++ ethMnemonic/MnemGen/static/EthAddressToMnemonic.js
@@ -1,19 +1,24 @@
 $('#EthIn').on('submit', function(e){
 e.preventDefault();
-$.ajax({
-    type : "POST",
-    url: post_link_1,
-    data: {
-        addrss : $('#ethAddress').val(),
-        csrfmiddlewaretoken: csrf_token_1,
-        dataType: "json",
-    },
-    success: function(data){
-        document.getElementById("MnemonicOutput").textContent = data.msg;
-        document.getElementById("MnemonicOutput").style = "font-size: 20px;";
-        document.getElementById("copyAddressButton").style.visibility = 'visible';
-    },
-    failure: function() {
-    }
-        });
+    $.ajax({
+        type: "POST",
+        url: post_link_1,
+        data: {
+            addrss: $('#ethAddress').val(),
+            csrfmiddlewaretoken: csrf_token_1,
+            dataType: "json",
+        },
+        success: function (data) {
+            if (document.getElementById("validicon").style.display == 'block'){
+                    document.getElementById("MnemonicOutput").textContent = data.msg;
+                    document.getElementById("copyAddressButton").style.visibility = 'visible';
+                }
+                else{
+                    document.getElementById("MnemonicOutput").textContent = 'Invalid Address';
+                }
+            document.getElementById("MnemonicOutput").style = "font-size: 20px;";
+        },
+        failure: function () {
+        }
+    });
 });
--- ethMnemonic/MnemGen/static/MnemonicToEthAddress.js
+++ ethMnemonic/MnemGen/static/MnemonicToEthAddress.js
@@ -1,19 +1,24 @@
 $('#EthOut').on('submit', function(e){
-e.preventDefault();
-$.ajax({
-    type : "POST",
-    url: post_link_2,
-    data: {
-        mnem : $('#mnemonic').val(),
-        csrfmiddlewaretoken: csrf_token_2,
-        dataType: "json",
-    },
-    success: function(data){
-        document.getElementById("copyMnemButton").style.visibility = 'visible';
-        document.getElementById("AddressOutput").textContent = data.msg;
-        document.getElementById("AddressOutput").style = "font-size: 20px;";
-    },
-    failure: function() {
-    }
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: post_link_2,
+            data: {
+                mnem: $('#mnemonic').val(),
+                csrfmiddlewaretoken: csrf_token_2,
+                dataType: "json",
+            },
+            success: function (data) {
+                if (document.getElementById("validicon2").style.display == 'block'){
+                    document.getElementById("AddressOutput").textContent = data.msg;
+                    document.getElementById("copyMnemButton").style.visibility = 'visible';
+                }
+                else{
+                    document.getElementById("AddressOutput").textContent = 'Invalid Mnemonic';
+                }
+                document.getElementById("AddressOutput").style = "font-size: 20px;";
+            },
+            failure: function () {
+            }
         });
 });
